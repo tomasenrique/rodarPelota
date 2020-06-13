8469f1d652e67082787745a087550aba2bd4fbea
Tomas Estrada <tomasenriquea@hotmail.com>
2020-06-13 22:09:58 +0200

    Se agrega obstaculo de tipo cubo y se le aplica movimiento con la clase GiroCubo

---
+++ Assets/codigo/GiroCubo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiroCubo : MonoBehaviour
+{
+    private Vector3 rotacion = new Vector3(15, 30, 45); // Rotacion para el cubo
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Time.deltaTime almacena el tiempo en segundo para un fotograma, es para saber cuento pasa entre un fotograma y otro
+        transform.Rotate(rotacion * Time.deltaTime);
+    }
+}
