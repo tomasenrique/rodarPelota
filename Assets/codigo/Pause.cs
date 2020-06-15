297f398c74bb0e23bbc6eba417517a3fd4f7918b
Tomas Estrada <tomasenriquea@hotmail.com>
2020-06-16 00:26:25 +0200

    Se configura la tecla escape para salir del juego.

---
+++ Assets/codigo/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        // Este metodo es para la pausa del juego
+        if (Input.GetKeyDown(KeyCode.Escape)) // cuando se presiona la tecla 'Escape', y se asigna a cualquier objeto en la vista de unity
+        {
+            Application.Quit(); // en este caso se le asignara a el jugador
+        }
+
+        
+    }
+}
